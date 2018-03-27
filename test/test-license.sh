ad41d54bdae8d1fe95a63d8affc639ed8160a75e
Herbert Shen <ishbguy@hotmail.com>
2018-03-27 22:02:25 +0800

    fix: test opt -l -n -y fail in travis build

--- test/test-license.sh
+++ test/test-license.sh
@@ -11,13 +11,14 @@ trap 'rm -rf test-license' EXIT SIGINT ERR
 run_ok '$status -eq 1 && $output == "Please give a license."' license
 run_ok '$status -eq 0' license -h
 run_ok '$status -eq 0' license -v
-run_ok '$status -eq 0' license -l
+run_ok '$status -eq 1 && $output =~ "not a directory"' license -l
 run_ok '$status -eq 0' license -d test-license -u
 run_ok '$status -eq 0 && $(echo $output | wc -l) -gt 0' license -d test-license -l
-run_ok '$status -eq 0 && $output =~ "XXXXXX"' license -d test-license mit
-run_ok '$status -eq 0 && $output =~ "XXXXXX"' license -d test-license unlicense
+run_ok '$status -eq 0' license -d test-license mit
+run_ok '$status -eq 0' license -d test-license gpl-3.0
 run_ok '$status -eq 0 && $output =~ "XXXXXX"' license -d test-license -n XXXXXX mit
 run_ok '$status -eq 0 && $output =~ "XXXXXX"' license -d test-license -y XXXXXX mit
 run_ok '$status -eq 0 && -s test-license/test-mit' license -d test-license -o test-license/test-mit mit
+run_ok '$status -eq 1 && $output =~ "already exists"' license -d test-license -o test-license/test-mit mit
 
 summary
