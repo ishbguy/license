ac261bee468e356c46797473fbf8da99e39fec14
Herbert Shen <ishbguy@hotmail.com>
2018-05-07 20:06:05 +0800

    test: update test with using baux-test.sh

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
-TEST_DIR := $(shell pwd)/test
+UNIT_TEST := $(PWD)/baux/lib-exec/baux-test.sh
+TEST_DIR := $(PWD)/test
 
 all : test
 
 .PHONY : test
 test :
-	$(TEST_DIR)/test-license.sh
+	$(UNIT_TEST) $(TEST_DIR)
--- test/test-license.sh
+++ test/test-license.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env bash
 
-declare -gr LICENSE_TEST_ABS_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+#! /usr/bin/env bash
+# Copyright (c) 2018 Herbert Shen <ishbguy@hotmail.com> All Rights Reserved.
+# Released under the terms of the MIT License.
 
-source "$LICENSE_TEST_ABS_DIR/../license.sh"
-import "$LICENSE_TEST_ABS_DIR/../baux/lib/test.sh"
+# source guard
+[[ $TEST_LICENSE_SOURCED -eq 1 ]] && return
+declare -gr TEST_LICENSE_SOURCED=1
+declare -gr TEST_LICENSE_ABS_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-mkdir -p test-license || die "Can not mkdir."
-trap 'rm -rf test-license' EXIT SIGINT
+source "$TEST_LICENSE_ABS_DIR/../license.sh"
+import "$TEST_LICENSE_ABS_DIR/../baux/lib/test.sh"
 
-run_ok '$status -eq 1 && $output == "Please give a license."' license
-run_ok '$status -eq 0' license -h
-run_ok '$status -eq 0' license -v
-run_ok '$status -eq 1 && $output =~ "not a directory"' license -d dir -l
-run_ok '$status -eq 0' license -d test-license -u
-run_ok '$status -eq 0 && $(echo $output | wc -l) -gt 0' license -d test-license -l
-run_ok '$status -eq 0' license -d test-license mit
-run_ok '$status -eq 0' license -d test-license gpl-3.0
-run_ok '$status -eq 0 && $output =~ "XXXXXX"' license -d test-license -n XXXXXX mit
-run_ok '$status -eq 0 && $output =~ "XXXXXX"' license -d test-license -y XXXXXX mit
-run_ok '$status -eq 0 && -s test-license/test-mit' license -d test-license -o test-license/test-mit mit
-run_ok '$status -eq 1 && $output =~ "already exists"' license -d test-license -o test-license/test-mit mit
+test_license() {
+    mkdir -p test-license || die "Can not mkdir."
+    trap 'rm -rf test-license' RETURN EXIT SIGINT
 
-summary
+    run_ok '$status -eq 1 && $output == "Please give a license."' license
+    run_ok '$status -eq 0' license -h
+    run_ok '$status -eq 0' license -v
+    run_ok '$status -eq 0 && $output =~ "choosealicense"' license -c
+    run_ok '$status -eq 1 && $output =~ "not a directory"' license -d dir -l
+    run_ok '$status -eq 0' license -d test-license -u
+    run_ok '$status -eq 0 && $(echo $output | wc -l) -gt 0' license -d test-license -l
+    run_ok '$status -eq 0' license -d test-license mit
+    run_ok '$status -eq 0' license -d test-license gpl-3.0
+    run_ok '$status -eq 0 && $output =~ "XXXXXX"' license -d test-license -n XXXXXX mit
+    run_ok '$status -eq 0 && $output =~ "XXXXXX"' license -d test-license -y XXXXXX mit
+    run_ok '$status -eq 0 && -s test-license/test-mit' license -d test-license -o test-license/test-mit mit
+    run_ok '$status -eq 1 && $output =~ "already exists"' license -d test-license -o test-license/test-mit mit
+}
+
+[[ ${FUNCNAME[0]} == "main" ]] \
+    && test_license "$@" && summary
+
+# vim:set ft=sh ts=4 sw=4:
